676930cfe23a4f24b3c056e41699d2564e938a5f
Alex Murray <Alexander-A.Murray@aia.com>
2022-07-29 15:20:53 +1000

    Tidying up dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,10 @@
-import * as _ from "lodash";
-
 import { render } from "react-dom";
 
 import "./style.css";
 import { shout } from "./shouting";
 
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack", "with typescript"], " ");
-  element.classList.add("hello");
-  shout();
-
-  return element;
-}
-
 const element = document.createElement("div");
 document.body.appendChild(element);
 
 render(<h1>Hello! I am React in Typescript.</h1>, element);
+shout();
