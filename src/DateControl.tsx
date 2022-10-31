4d8053d1900864baec38979561ced75fe05d8907
Alex Murray <Alexander-A.Murray@aia.com>
2022-10-31 15:56:43 +1100

    No backspace, no arrows, but basically pleasant to use.

--- src/DateControl.tsx
+++ src/DateControl.tsx
@@ -60,20 +60,17 @@ export const DateControl = (props: IDateControlProps) => {
           let newDayValue = e.target.value.replace(/\D/g, "");
           let newMonthValue: string = props.month;
           let newYearValue: string = props.year;
+          let overflow = false;
           const currentFieldContent = props.day;
 
           console.log(">>", dayRef.current?.selectionStart);
           const selectionStart = dayRef.current?.selectionStart;
 
           if (newDayValue.length === 3) {
-            //push the character into the next field and focus it? Can we?
-            newMonthValue = `${newDayValue.slice(2)}${props.month}`;
-            if (newMonthValue.length === 3) {
-              newYearValue = `${newMonthValue.slice(2)}${props.year}`.slice(
-                0,
-                4
-              );
-              newMonthValue = newMonthValue.slice(0, 2);
+            if (props.month.length === 0) {
+              //push the character into the next field and focus it? Can we?
+              newMonthValue = newDayValue.slice(2);
+              overflow = true;
             }
             newDayValue = newDayValue.slice(0, 2);
           }
@@ -82,7 +79,7 @@ export const DateControl = (props: IDateControlProps) => {
             `dispatching ${newDayValue}:${newMonthValue}:${newYearValue}`
           );
 
-          if (selectionStart === 3) {
+          if (selectionStart === 3 && overflow) {
             monthRef.current?.focus();
           }
           props.onBulkChange(newDayValue, newMonthValue, newYearValue);
@@ -105,13 +102,17 @@ export const DateControl = (props: IDateControlProps) => {
           let newDayValue: string = props.day;
           let newMonthValue: string = e.target.value.replace(/\D/g, "");
           let newYearValue: string = props.year;
+          let overflow = false;
           const currentFieldContent = props.month;
 
           console.log("++", dayRef.current?.selectionStart);
           const selectionStart = monthRef.current?.selectionStart;
 
           if (newMonthValue.length === 3) {
-            newYearValue = `${newMonthValue.slice(2)}${props.year}`.slice(0, 4);
+            if (props.year.length === 0) {
+              newYearValue = newMonthValue.slice(2);
+              overflow = true;
+            }
             newMonthValue = newMonthValue.slice(0, 2);
           }
 
@@ -119,7 +120,7 @@ export const DateControl = (props: IDateControlProps) => {
             `dispatching ${newDayValue}:${newMonthValue}:${newYearValue}`
           );
 
-          if (selectionStart === 3) {
+          if (selectionStart === 3 && overflow) {
             yearRef.current?.focus();
           }
           props.onBulkChange(newDayValue, newMonthValue, newYearValue);
