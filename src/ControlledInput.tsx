02c26218ad3677b3d02ea489fd2e41d7b971ffba
Alex Murray <Alexander-A.Murray@aia.com>
2022-11-01 22:41:54 +1100

    Some annotations to the copied code.

--- src/ControlledInput.tsx
+++ src/ControlledInput.tsx
@@ -5,15 +5,18 @@ export const ControlledInput = (props: {
   onChange: (e: string) => void;
 }) => {
   const { value, onChange, ...rest } = props;
-  const [cursor, setCursor] = useState(null);
+  const [cursor, setCursor] = useState<number>(null);
   const ref = useRef(null);
 
   useEffect(() => {
+    console.log(">>", cursor, value);
     const input = ref.current;
+    // restore cursor position after render, if cursor, input value or input control have changed.
     if (input) input.setSelectionRange(cursor, cursor);
   }, [ref, cursor, value]);
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    // Set cursor position onChange...
     setCursor(e.target.selectionStart);
     onChange && onChange(e.target.value);
   };
