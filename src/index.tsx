ec5d8880a2d7a65e9b798668bd7131589fe1e021
Alex Murray <Alexander-A.Murray@aia.com>
2022-11-28 12:01:06 +1100

    Using calc with custom properties.

--- src/index.tsx
+++ src/index.tsx
@@ -8,5 +8,19 @@ const container = document.createElement("div");
 document.body.appendChild(container);
 
 const root = createRoot(container);
-root.render(<h1 className="hello">Hello! I am React in Typescript.</h1>);
+root.render(
+  <div>
+    <h1 className="hello">Hello! I am React in Typescript.</h1>
+    <p>
+      Native CSS variables - OK, custom properties - start with <code>--</code>{" "}
+      and are accessed via <code>var()</code>. They are declared inside elements
+      and can be overridden. For globally accessible variables, declare them in
+      the <code>:root</code> pseudo-element.
+    </p>
+    <p>
+      It's quite possible to do arithmetic entirely with variable values, using
+      the <code>calc</code> function.
+    </p>
+  </div>
+);
 shout();
