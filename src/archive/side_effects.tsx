b2901133bb3c7c4cae36c6a50763e2990a9354da
Alex Murray <Alex.Murray1@coles.com.au>
2024-06-24 15:17:07 +1000

    Math and logic exercises

---
+++ src/archive/side_effects.tsx
@@ -0,0 +1,173 @@
+import { createRoot } from "react-dom/client";
+import React, { useState } from "react";
+import "./style.css";
+
+// const element = document.createElement("div");
+// document.body.appendChild(element);
+
+const Container = () => {
+  // write a function that returns nothing, but sets the
+  // value of the local variable 'answer1' to a specific number.
+
+  let answer1: number;
+
+  function one() {
+    answer1 = 1;
+  }
+  one();
+  console.log("this is answer1", answer1);
+
+  // write a function that returns nothing, but sets the
+  // value of the local variable 'answer2' to the value of its single
+  // string argument.
+
+  let answer2: string;
+
+  function two(answer: string) {
+    answer2 = answer;
+  }
+  two("hello");
+  console.log("this is answer2", answer2);
+
+  // write a function that returns nothing, but sets the
+  // value of the local variable 'answer3' to the value of the
+  // local variable 'sourceValue3'.
+
+  const sourceValue3 = "source value 3";
+  let answer3: string;
+
+  function three() {
+    answer3 = sourceValue3;
+  }
+  three();
+  console.log("this is answer3", answer3);
+
+  // write a function that returns nothing, but increments the local
+  // variable 'answer4' by 10. Update the arrow function clickHandler4
+  // to execute this function.
+
+  let answer4 = 10;
+  function four() {
+    answer4 = answer4 + 10;
+  }
+  const clickHandler4: () => void = () => four();
+
+  console.log("this is answer4", answer4);
+
+  // write a function that returns nothing, but increments the local
+  // variable 'answer5' by 100. Update the arrow function
+  // incrementClickHandler5 to execute this function.
+
+  // write another function that returns nothing, but decrements 'answer5'
+  // by 19. Update the arrow function decrementClickHandler5
+  // to execute this function.
+
+  let answer5 = 50;
+  function five() {
+    answer5 = answer5 + 10;
+  }
+  const incrementClickHandler5: () => void = () => five();
+
+  function six() {
+    answer5 = answer5 - 19;
+  }
+  const decrementClickHandler5: () => void = () => six();
+
+  // write a function that takes a single function as an argument,
+  // and returns an object with two function properties
+  // called increment and decrement.
+
+  // but increments the local
+  // variable 'answer5' by 100. Update the arrow function
+  // incrementClickHandler5 to execute this function.
+
+  // write another function that returns nothing, but decrements 'answer5'
+  // by 19. Update the arrow function decrementClickHandler5
+  // to execute this function.
+
+  // Call the function and set 'incrementClickHandler6' and
+  // 'decrementClickHandler6' to each call one of the two functions
+  // your function returns.
+
+  // define your function here
+
+  function seven(lala: (newValue: number) => void) {
+    let internal = 10;
+    return {
+      increment: () => {
+        internal = internal + 1;
+        lala(internal);
+      },
+      decrement: () => {
+        internal = internal - 1;
+        lala(internal);
+      },
+    };
+  }
+
+  const [answer6, setAnswer6] = useState(10);
+  const callback6 = (newValue: number) => setAnswer6(newValue);
+
+  // call your function here
+  const lolo = seven(callback6);
+  const incrementClickHandler6: () => void = () => lolo.increment();
+  const decrementClickHandler6: () => void = () => lolo.decrement();
+
+  return (
+    <div className="container">
+      <h3>Named functions</h3>
+      <h4>1. Set a single local variable value</h4>
+      <p>{answer1}</p>
+      <h4>2. Set a single local variable value to the argument value</h4>
+      <p>{answer2}</p>
+      <h4>3. Set a single local variable value to another local variable</h4>
+      <p>{answer3}</p>
+      <h4>
+        4. Increment a single local variable value, and bind this function to a
+        button click event
+      </h4>
+      <p>
+        <input type="button" onClick={clickHandler4} value="click me" />
+        {answer4}
+      </p>
+      <h4>
+        5. Write a function to increment a single local variable value, and bind
+        this function to a button click event. Write another function to
+        decrement the same variable, and bind that function to a different
+        button click event.
+      </h4>
+      <p>
+        <input
+          type="button"
+          onClick={incrementClickHandler5}
+          value="increment"
+        />
+        &nbsp; {answer5} &nbsp;
+        <input
+          type="button"
+          onClick={decrementClickHandler5}
+          value="decrement"
+        />
+      </p>
+      <h4>
+        6. Do the same, but encapsulate everything inside a single function.
+      </h4>
+      <p>
+        <input
+          type="button"
+          onClick={incrementClickHandler6}
+          value="increment"
+        />
+        &nbsp; {answer6} &nbsp;
+        <input
+          type="button"
+          onClick={decrementClickHandler6}
+          value="decrement"
+        />
+      </p>
+    </div>
+  );
+};
+const domNode = document.getElementsByTagName("body");
+const root = createRoot(domNode[0]);
+root.render(<Container />);
--- src/index.tsx
+++ src/index.tsx
@@ -2,168 +2,226 @@ import { createRoot } from "react-dom/client";
 import React, { useState } from "react";
 import "./style.css";
 
-// const element = document.createElement("div");
-// document.body.appendChild(element);
-
 const Container = () => {
-  // write a function that returns nothing, but sets the
-  // value of the local variable 'answer1' to a specific number.
+  // 1.
+  const [input1, setInput1] = useState(0);
+  const [answer1, setAnswer1] = useState(0);
 
-  let answer1: number;
+  // Define a function called 'one' that it returns the number it receives as an argument or
+  // the number 49, whichever is larger.
 
-  function one() {
-    answer1 = 1;
-  }
-  one();
-  console.log("this is answer1", answer1);
+  const click1 = () => setAnswer1(one(input1));
 
-  // write a function that returns nothing, but sets the
-  // value of the local variable 'answer2' to the value of its single
-  // string argument.
+  // 2.
+  const [input2, setInput2] = useState(0);
+  const [answer2, setAnswer2] = useState(0);
 
-  let answer2: string;
+  // Define a function called 'two' that returns the number it receives as an argument or the
+  // number 512, whichever is smaller.
 
-  function two(answer: string) {
-    answer2 = answer;
-  }
-  two("hello");
-  console.log("this is answer2", answer2);
+  const click2 = () => setAnswer2(two(input2));
 
-  // write a function that returns nothing, but sets the
-  // value of the local variable 'answer3' to the value of the
-  // local variable 'sourceValue3'.
+  // 3.
+  const [input3a, setInput3a] = useState(0);
+  const [input3b, setInput3b] = useState(0);
+  const [answer3, setAnswer3] = useState(0);
 
-  const sourceValue3 = "source value 3";
-  let answer3: string;
+  // Define a function called 'three' that it returns the largest of the two numbers it receives as
+  // arguments.
 
-  function three() {
-    answer3 = sourceValue3;
-  }
-  three();
-  console.log("this is answer3", answer3);
+  const click3 = () => setAnswer3(three(input3a, input3b));
 
-  // write a function that returns nothing, but increments the local
-  // variable 'answer4' by 10. Update the arrow function clickHandler4
-  // to execute this function.
+  // 4.
+  const [input4a, setInput4a] = useState(0);
+  const [input4b, setInput4b] = useState(0);
+  const [answer4, setAnswer4] = useState(0);
 
-  let answer4 = 10;
-  function four() {
-    answer4 = answer4 + 10;
-  }
-  const clickHandler4: () => void = () => four();
+  // Define a function called 'four' that it returns the smallest of the two numbers it receives
+  // as arguments.
 
-  console.log("this is answer4", answer4);
+  const click4 = () => setAnswer4(four(input4a, input4b));
+  // 5.
+  const [input5, setInput5] = useState(0);
+  const [answer5, setAnswer5] = useState(0);
 
-  // write a function that returns nothing, but increments the local
-  // variable 'answer5' by 100. Update the arrow function
-  // incrementClickHandler5 to execute this function.
+  // Define a function called 'five' that it randomly returns either 1 (75% of the time) or -1
+  // (25% of the time).
 
-  // write another function that returns nothing, but decrements 'answer5'
-  // by 19. Update the arrow function decrementClickHandler5
-  // to execute this function.
+  const click5 = () => setAnswer5(five(input5));
+  // 6.
+  const [input6, setInput6] = useState("");
+  const [answer6, setAnswer6] = useState("");
 
-  let answer5 = 50;
-  function five() {
-    answer5 = answer5 + 10;
-  }
-  const incrementClickHandler5: () => void = () => five();
+  // Define a function called 'six' that it returns the number of times the lowercase letter e
+  // appears in the string it receives as an argument.
 
-  function six() {
-    answer5 = answer5 - 19;
-  }
-  const decrementClickHandler5: () => void = () => six();
+  const click6 = () => setAnswer6(six(input6));
+  // 7.
+  const [input7, setInput7] = useState("");
+  const [answer7, setAnswer7] = useState("");
 
-  // write a function that takes a single function as an argument,
-  // and returns an object with two function properties
-  // called increment and decrement.
+  // Define a function called 'seven' that it returns the string it receives as an argument, but
+  // with the first three characters and last three characters removed.
 
-  // but increments the local
-  // variable 'answer5' by 100. Update the arrow function
-  // incrementClickHandler5 to execute this function.
+  const click7 = () => setAnswer7(seven(input7));
+  // 8.
+  const [input8, setInput8] = useState("");
+  const [answer8, setAnswer8] = useState("");
 
-  // write another function that returns nothing, but decrements 'answer5'
-  // by 19. Update the arrow function decrementClickHandler5
-  // to execute this function.
+  // Define a function called 'eight' that it splits the string it receives as an argument into
+  // words at whitespace boundaries, and returns one word chosen at random,
+  // repeated a random number of times from 1 to 10
 
-  // Call the function and set 'incrementClickHandler6' and
-  // 'decrementClickHandler6' to each call one of the two functions
-  // your function returns.
+  const click8 = () => setAnswer8(eight(input8));
+  // 9.
+  const [input9, setInput9] = useState("");
+  const [answer9, setAnswer9] = useState("");
 
-  // define your function here
+  // Define a function called 'nine' that takes a string argument and a number.
+  // The function should return all the nth characters of the string, where n is
+  // the value of the number argument.
 
-  function seven(lala: (newValue: number) => void) {
-    let internal = 10;
-    return {
-      increment: () => {
-        internal = internal + 1;
-        lala(internal);
-      },
-      decrement: () => {
-        internal = internal - 1;
-        lala(internal);
-      },
-    };
-  }
-
-  const [answer6, setAnswer6] = useState(10);
-  const callback6 = (newValue: number) => setAnswer6(newValue);
-
-  // call your function here
-  const lolo = seven(callback6);
-  const incrementClickHandler6: () => void = () => lolo.increment();
-  const decrementClickHandler6: () => void = () => lolo.decrement();
-
+  const click9 = () => setAnswer9(nine(input9));
   return (
     <div className="container">
-      <h3>Named functions</h3>
-      <h4>1. Set a single local variable value</h4>
-      <p>{answer1}</p>
-      <h4>2. Set a single local variable value to the argument value</h4>
-      <p>{answer2}</p>
-      <h4>3. Set a single local variable value to another local variable</h4>
-      <p>{answer3}</p>
+      <h3>Math and logic</h3>
       <h4>
-        4. Increment a single local variable value, and bind this function to a
-        button click event
+        1. A function that returns the number it receives as an argument or the
+        number 49, whichever is larger.
       </h4>
       <p>
-        <input type="button" onClick={clickHandler4} value="click me" />
-        {answer4}
+        <input
+          type="text"
+          value={input1}
+          onChange={(e) => setInput1(parseInt(e.target.value))}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click1} value="execute" /> &nbsp;
+        {"" + answer1}
       </p>
       <h4>
-        5. Write a function to increment a single local variable value, and bind
-        this function to a button click event. Write another function to
-        decrement the same variable, and bind that function to a different
-        button click event.
+        2. A function that returns the number it receives as an argument or the
+        number 512, whichever is smaller.
       </h4>
       <p>
         <input
-          type="button"
-          onClick={incrementClickHandler5}
-          value="increment"
-        />
-        &nbsp; {answer5} &nbsp;
-        <input
-          type="button"
-          onClick={decrementClickHandler5}
-          value="decrement"
-        />
+          type="text"
+          value={input2}
+          onChange={(e) => setInput2(parseInt(e.target.value))}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click2} value="execute" /> &nbsp;
+        {"" + answer2}
       </p>
       <h4>
-        6. Do the same, but encapsulate everything inside a single function.
+        3. A function that returns the largest of the two numbers it receives as
+        arguments.
       </h4>
       <p>
         <input
-          type="button"
-          onClick={incrementClickHandler6}
-          value="increment"
-        />
-        &nbsp; {answer6} &nbsp;
+          type="text"
+          value={input3a}
+          onChange={(e) => setInput3a(parseInt(e.target.value))}
+        />{" "}
         <input
-          type="button"
-          onClick={decrementClickHandler6}
-          value="decrement"
-        />
+          type="text"
+          value={input3b}
+          onChange={(e) => setInput3b(parseInt(e.target.value))}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click3} value="execute" /> &nbsp;
+        {"" + answer3}
+      </p>
+      <h4>
+        4. A function that returns the smallest of the two numbers it receives
+        as arguments.
+      </h4>
+      <p>
+        <input
+          type="text"
+          value={input4a}
+          onChange={(e) => setInput4a(parseInt(e.target.value))}
+        />{" "}
+        <input
+          type="text"
+          value={input4b}
+          onChange={(e) => setInput4b(parseInt(e.target.value))}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click4} value="execute" /> &nbsp;
+        {"" + answer4}
+      </p>
+      <h4>
+        5. A function that randomly returns either 1 (75% of the time) or -1
+        (25% of the time).
+      </h4>
+      <p>
+        <input
+          type="text"
+          value={input5}
+          onChange={(e) => setInput5(parseInt(e.target.value))}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click5} value="execute" /> &nbsp;
+        {"" + answer5}
+      </p>
+      <h4>
+        6. A function that returns the number of times the lowercase letter e
+        appears in the string it receives as an argument.
+      </h4>
+      <p>
+        <input
+          type="text"
+          value={input6}
+          onChange={(e) => setInput6(e.target.value)}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click6} value="execute" /> &nbsp;
+        {"" + answer6}
+      </p>
+      <h4>
+        7. A function that returns the string it receives as an argument, but
+        with the first three characters and last three characters removed.
+      </h4>
+      <p>
+        <input
+          type="text"
+          value={input7}
+          onChange={(e) => setInput7(e.target.value)}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click7} value="execute" /> &nbsp;
+        {"" + answer7}
+      </p>
+      <h4>
+        8. A function that splits the string it receives as an argument into
+        words at whitespace boundaries, and returns one word chosen at random,
+        repeated a random number of times from 1 to 10
+      </h4>
+      <p>
+        <input
+          type="text"
+          value={input8}
+          onChange={(e) => setInput8(e.target.value)}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click8} value="execute" /> &nbsp;
+        {"" + answer8}
+      </p>
+      <h4>
+        9. A function that that takes a string argument and a number. The
+        function should return all the nth characters of the string, where n is
+        the value of the number argument.
+      </h4>
+      <p>
+        <input
+          type="text"
+          value={input9}
+          onChange={(e) => setInput9(e.target.value)}
+        />{" "}
+        &nbsp;
+        <input type="button" onClick={click9} value="execute" /> &nbsp;
+        {"" + answer9}
       </p>
     </div>
   );
