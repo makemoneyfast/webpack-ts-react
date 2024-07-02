8030aae85e670dfc9dc17988cd3c9ac7bbc6a5ce
Elysia Teo <elysia.teo@coles.com.au>
2024-07-02 17:07:49 +1000

    array manipulation lesson

--- src/index.tsx
+++ src/index.tsx
@@ -9,33 +9,79 @@ const Container = () => {
   console.log("* - ".repeat(10));
   console.log("source array 1: ", sourceArray1);
   console.log("* - ".repeat(10));
+
   console.log(
-    "Write a function that takes an array of numbers and returns an array of the same length where every entry is eqiuvalent to the source array value plus 6."
+    "Write a function that takes an array of numbers and returns an array of the same length where every entry is eqiuvalent to the source array value plus 6.", one()
   );
+
+  function one() {
+    return sourceArray1.map((x) => x + 6);
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns a new array containing the same numbers with any fractional quantities removed."
+    "Write a function that takes an array of numbers and returns a new array containing the same numbers with any fractional quantities removed.", two()
   );
+
+  function two() {
+    return sourceArray1.map((x) => Math.floor(x));
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns a new array containing only the fractional quantities of the input numbers."
+    "Write a function that takes an array of numbers and returns a new array containing only the fractional quantities of the input numbers.", three()
   );
+
+  function three() {
+    return sourceArray1.map((x) => x % 1);
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns a new array of the same numbers in decreasing order"
+    "Write a function that takes an array of numbers and returns a new array of the same numbers in decreasing order", four()
   );
+
+  function four() {
+    return [...sourceArray1].sort((a: number, b: number) => b - a);
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns a new array of the same numbers in decreasing order of absolute value"
+    "Write a function that takes an array of numbers and returns a new array of the same numbers in decreasing order of absolute value", five()
   );
+
+  function five() {
+    return [...sourceArray1].sort((a: number, b: number) => Math.abs(b) - Math.abs(a));
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns a new array of the same numbers as strings"
+    "Write a function that takes an array of numbers and returns a new array of the same numbers as strings", six()
   );
+
+  function six() {
+    return sourceArray1.map((x) => x.toString());
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns an array of boolean values (true for fractional numbers, false for integers)"
+    "Write a function that takes an array of numbers and returns an array of boolean values (true for fractional numbers, false for integers", seven()
   );
+  
+  function seven() {
+    return sourceArray1.map((x) => x % 1 !== 0 );
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns the sum of all numbers in the array"
+    "Write a function that takes an array of numbers and returns the sum of all numbers in the array", eight()
   );
+
+  function eight() {
+    return sourceArray1.reduce((accumulator, x) => accumulator + x, 0);
+  }
+
   console.log(
-    "Write a function that takes an array of numbers and returns the sum of all the even-numbered entries in the array"
+    "Write a function that takes an array of numbers and returns the sum of all the even-numbered entries in the array", nine()
   );
+
+  function nine() {
+    return sourceArray1.reduce((accumulator, x, i) => i % 2 !== 0 ? accumulator + x : accumulator, 0);
+  }
+
   console.log(
     "Write a function that takes an array of numbers and returns the sum of all the even-numbered entries in the array"
   );
