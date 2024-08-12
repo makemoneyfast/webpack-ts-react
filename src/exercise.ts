3d514a94870eb8793ca3d1d8874aec13157bac1b
Alex Murray <Alex.Murray1@coles.com.au>
2024-08-12 10:17:59 +1000

    broken on purpose

--- src/exercise.ts
+++ src/exercise.ts
@@ -8,38 +8,4 @@ type CreatePresence = () => {
   getDescription: () => string;
 };
 
-export const createPresence: CreatePresence = (
-  followersInput?: number,
-  postsInput?: number,
-  nameInput?: string
-) => {
-  let posts = postsInput || 0;
-  let followers = followersInput || 0;
-  let name = nameInput || "anonymous";
-  const setPosts = (a: number) => {
-    posts = a;
-  };
-  const getPosts = () => posts;
-  const setFollowers = (a: number) => {
-    followers = a;
-  };
-  const getFollowers = () => followers;
-  const setName = (b: string) => {
-    name = b;
-  };
-  const getName = () => name;
-  const getDescription = () =>
-    `With ${posts} posts to ${followers} followers, ${name} has approximately ${
-      posts * followers
-    } clout.`;
-
-  return {
-    setPosts,
-    getPosts,
-    setName,
-    getName,
-    setFollowers,
-    getFollowers,
-    getDescription,
-  };
-};
+export const createPresence: CreatePresence = () => {};
