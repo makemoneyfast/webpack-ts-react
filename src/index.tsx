52a5b6d9ef4a5459fe8d3571f121f6457b5e5395
Alex Murray <Alex.Murray1@coles.com.au>
2024-09-18 17:20:27 +1000

    lolol

--- src/index.tsx
+++ src/index.tsx
@@ -26,10 +26,20 @@ const embassy = () => {
 
     const diplomat = (b: string, l: string, d: string) => {
       console.log(`Breakfast is ${breakfast.food} at ${b}`);
-      console.log(`Lunch is ${breakfast.food} at ${l}`);
+      console.log(`Lunch is ${lunch.food} at ${l}`);
       console.log(`Dinner is ${dinner.food} at ${d}`);
     };
 
     setTimeout(() => diplomat(breakfastTime, lunchTime, dinnerTime));
+
+    return diplomat;
   };
+
+  return bedroom();
 };
+
+console.log("hi");
+
+const theDiplomat = embassy();
+
+theDiplomat("0900", "1200", "1800");
