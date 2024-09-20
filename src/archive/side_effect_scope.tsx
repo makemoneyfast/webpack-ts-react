e4707dc45a1b8e4a412a168241b27dad292cf703
Elysia Teo <elysia.teo@coles.com.au>
2024-09-20 16:52:46 +1000

    update

--- src/archive/side_effect_scope.tsx
+++ src/archive/side_effect_scope.tsx
@@ -1,45 +1,45 @@
-import { createRoot } from "react-dom/client";
-import React, { useState } from "react";
-import "./style.css";
+// import { createRoot } from "react-dom/client";
+// import React, { useState } from "react";
+// import "./style.css";
 
-const breakfast = {
-  food: "ancient grains with organic yoghurt and artisanal fruit selection",
-};
+// const breakfast = {
+//   food: "ancient grains with organic yoghurt and artisanal fruit selection",
+// };
 
-let breakfastTime = "0900";
+// let breakfastTime = "0900";
 
-const embassy = () => {
-  const lunch = {
-    food: "a selection of locally-sourced charcuterie",
-  };
+// const embassy = () => {
+//   const lunch = {
+//     food: "a selection of locally-sourced charcuterie",
+//   };
 
-  let lunchTime = "1200";
+//   let lunchTime = "1200";
 
-  const bedroom = () => {
-    let dinnerTime = "1200";
+//   const bedroom = () => {
+//     let dinnerTime = "1200";
 
-    const dinner = {
-      food: "",
-    };
+//     const dinner = {
+//       food: "",
+//     };
 
-    let dinnerTime = "1200";
+//     let dinnerTime = "1200";
 
-    const diplomat = (b: string, l: string, d: string) => {
-      console.log(`Breakfast is ${breakfast.food} at ${b}`);
-      console.log(`Lunch is ${lunch.food} at ${l}`);
-      console.log(`Dinner is ${dinner.food} at ${d}`);
-    };
+//     const diplomat = (b: string, l: string, d: string) => {
+//       console.log(`Breakfast is ${breakfast.food} at ${b}`);
+//       console.log(`Lunch is ${lunch.food} at ${l}`);
+//       console.log(`Dinner is ${dinner.food} at ${d}`);
+//     };
 
-    setTimeout(() => diplomat(breakfastTime, lunchTime, dinnerTime));
+//     setTimeout(() => diplomat(breakfastTime, lunchTime, dinnerTime));
 
-    return diplomat;
-  };
+//     return diplomat;
+//   };
 
-  return bedroom();
-};
+//   return bedroom();
+// };
 
-console.log("hi");
+// console.log("hi");
 
-const theDiplomat = embassy();
+// const theDiplomat = embassy();
 
-theDiplomat("0900", "1200", "1800");
+// theDiplomat("0900", "1200", "1800");
