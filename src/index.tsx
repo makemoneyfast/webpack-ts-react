0e1fd9ed68203f6fff21354e6e487d0eff08696b
Elysia Teo <elysia.teo@coles.com.au>
2024-10-15 17:05:41 +1100

    lesson

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,27 @@ const noticeMe = () => alert("notice me senpai");
 // And then this one
 const noticeMeHarder = () => confirm("notice me senpai");
 
+const noticeMeTimed = () => setTimeout(noticeMe, 5000);
+
+const noticeMeInt = () => intervalID = setInterval(noticeMe, 5000);
+
+const stopNoticingMe = () => clearInterval(intervalID)
+
+let intervalID: any
+
 const Container = () => {
   return (
     <div className="container">
       <h2>Momo</h2>
-      <img src="https://a.storyblok.com/f/178900/1060x596/4663a45721/dandadan-teaser-trailer-momo.jpg/m/filters:quality(95)format(webp)" />
-      <p>
+      <img onCopy={noticeMe} src="https://a.storyblok.com/f/178900/1060x596/4663a45721/dandadan-teaser-trailer-momo.jpg/m/filters:quality(95)format(webp)" />
+      <p onDrag={() => console.log("paragraph")}>
         Momo Ayase is a high school girl who believes in ghosts but not aliens,
         while her classmate Ken Takakura, whom Momo nicknames "Okarun", believes
         in aliens but not ghosts.
       </p>
       <label htmlFor="message">Say something to Momo</label>{" "}
       <input id="message" type="text" />
-      <input type="button" value="send" />
+      <input onClick={stopNoticingMe} type="button" value="send" />
     </div>
   );
 };
@@ -26,3 +34,4 @@ const domNode = document.getElementsByTagName("body");
 const root = createRoot(domNode[0]);
 root.render(<Container />);
 noticeMe();
+noticeMeInt();
