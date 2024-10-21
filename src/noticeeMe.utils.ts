ec92f52815bc0d7a299cab0138c650a5dc87b391
Alex Murray <Alex.Murray1@coles.com.au>
2024-10-21 16:37:07 +1100

    promises

--- src/noticeeMe.utils.ts
+++ src/noticeeMe.utils.ts
@@ -73,3 +73,78 @@ export class NoticeMeUtils {
     }, 4000);
   }
 }
+
+export class NoticeMePromiseUtils {
+  constructor() {
+    this.phoneNumber = Math.floor(Math.random() * 1000000);
+    this.applePassword = Math.floor(Math.random() * 1000000) + "";
+    this.restaurantLat = Math.floor(Math.random() * 1000000);
+    this.restaurantLong = Math.floor(Math.random() * 1000000);
+  }
+
+  private phoneNumber: number;
+  private applePassword: string;
+  private restaurantLat: number;
+  private restaurantLong: number;
+
+  public getPhoneNumber() {
+    return new Promise<number>((resolve) => {
+      console.log("started finding out what senpai's phone number is");
+      setTimeout(() => {
+        console.log(`Got senpai's phone number, it's ${this.phoneNumber}`);
+        resolve(this.phoneNumber);
+      }, 4000);
+    });
+  }
+
+  public cloneSimCard(phoneNumber: number) {
+    return new Promise<string>((resolve, reject) => {
+      console.log("started cloning SIM card");
+      setTimeout(() => {
+        if (phoneNumber !== this.phoneNumber) {
+          reject(new Error("didn't work! wrong phone number"));
+        } else {
+          console.log(
+            `Got senpai's Apple ID password, it's ${this.applePassword}`
+          );
+          resolve(this.applePassword);
+        }
+      }, 4000);
+    });
+  }
+
+  public clonePhone(applePassword: string) {
+    return new Promise<{ lat: string; long: string }>((resolve, reject) => {
+      console.log("started cloning senpai's phone");
+      setTimeout(() => {
+        if (applePassword !== this.applePassword) {
+          reject(new Error("didn't work, wrong account password"));
+        } else {
+          console.log(
+            `Got senpai's favorite restaurant geolocation, it's latitude: ${this.restaurantLat} longitude: ${this.restaurantLong}`
+          );
+          resolve({ lat: this.restaurantLat, long: this.restaurantLong });
+        }
+      }, 4000);
+    });
+  }
+
+  public goToRestaurant(location: { lat: number; long: number }) {
+    return new Promise<void>((resolve, reject) => {
+      console.log(
+        "now travelling to probable coordinates of senpai's favourite restaurant"
+      );
+      setTimeout(() => {
+        if (
+          location.lat !== this.restaurantLat ||
+          location.long !== this.restaurantLong
+        ) {
+          reject(new Error("wrong restaurant, senpai is not here"));
+        } else {
+          console.log(`SENPAI NOTICED ME`);
+          resolve();
+        }
+      }, 4000);
+    });
+  }
+}
